048d9e706eb660fcebb27f571eb51a9a23ef3f1d
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2025-12-01 16:40:44 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import tkinter
+
+# Initializes main application window
+window = tkinter.Tk()
+window.title("DFA Generator")
+
+# Runs infinite loop to update status of main window
+window.mainloop()
