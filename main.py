4c8dfac7c52e1f0070dd6106b55a00f97ab2dac4
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2025-12-03 09:05:08 -0700

    Streamlined entry code

--- main.py
+++ main.py
@@ -13,8 +13,20 @@ def createStandardLabel(parent, text, row, column):
         font = ("Courier New", 10),
         pady = 10
     )
-
     label.grid(row = row, column = column, sticky = "w")
+    return label
+
+def createStandardEntry(parent, row, column):
+    entry = tkinter.Entry(parent)
+    entry.grid(row = row, column = column, sticky = "ew", padx = 10)
+    return entry
+
+def createStandardFrame(parent):
+    frame = tkinter.Frame(
+        parent,
+        bg = MAINBACKGROUND
+    )
+    return frame
 
 # Initializes main application window
 window = tkinter.Tk()
@@ -23,19 +35,12 @@ window.geometry("600x400")
 window.configure(bg = MAINBACKGROUND)
 
 # Initialize Main Frame
-frame = tkinter.Frame(
-    window,
-    bg = MAINBACKGROUND
-)
-
+frame = createStandardFrame(window)
 frame.pack(fill="both", expand=True)
 frame.columnconfigure(0, weight=1)
 
 # Initialize Header
-headerFrame = tkinter.Frame(
-    frame, 
-    bg = MAINBACKGROUND
-)
+headerFrame = createStandardFrame(frame)
 
 titleLabel = tkinter.Label(
     headerFrame, 
@@ -43,43 +48,29 @@ titleLabel = tkinter.Label(
     fg = "white", 
     bg = MAINBACKGROUND, 
     font = ("Courier New", 20, "bold"),
-    pady = 4
-)
-
-subtitleLabel = tkinter.Label(
-    headerFrame,
-    text = "Separate characters with commas (,)",
-    fg = "white",
-    bg = MAINBACKGROUND,
-    font = ("Courier New", 10)
+    pady = 5
 )
 
 headerFrame.grid(row = 0, column = 0)
 titleLabel.grid(row = 0, column = 0)
-subtitleLabel.grid(row = 1, column = 0)
+subtitleLabel = createStandardLabel(headerFrame, "Separate characters with commas (,)", 1, 0)
 
 # Initialize Formal Inputs Section
-inputsFrame = tkinter.Frame(
-    frame,
-    bg = MAINBACKGROUND
-)
-
-statesInput = tkinter.Entry(inputsFrame)
-
-alphabetInput = tkinter.Entry(inputsFrame)
-
-startStateInput = tkinter.Entry(inputsFrame)
+inputsFrame = createStandardFrame(frame)
 
 inputsFrame.grid(row = 1, column = 0, sticky = "ew")
 inputsFrame.columnconfigure(1, weight = 1)
 
 statesLabel = createStandardLabel(inputsFrame, "Input List of States", 0, 0)
-statesInput.grid(row = 0, column = 1, sticky = "ew", padx = 10)
+statesInput = createStandardEntry(inputsFrame, 0, 1)
 
 alphabetLabel = createStandardLabel(inputsFrame, "Input Alphabet", 1, 0)
-alphabetInput.grid(row = 1, column = 1, sticky="ew", padx=10)
+alphabetInput = createStandardEntry(inputsFrame, 1, 1)
 
 startStateLabel = createStandardLabel(inputsFrame, "Input Start State", 2, 0)
-startStateInput.grid(row = 2, column = 1, sticky="ew", padx=10)
+startStateInput = createStandardEntry(inputsFrame, 2, 1)
+
+acceptStatesLabel = createStandardLabel(inputsFrame, "Input Accept States", 3, 0)
+acceptStatesEntry = createStandardEntry(inputsFrame, 3, 1)
 
 window.mainloop()
