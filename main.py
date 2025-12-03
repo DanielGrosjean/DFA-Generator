933c7c75b3a310800a5baec02c8e59c73d9cf1ce
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2025-12-03 08:08:23 -0700

    cleaned up code

--- main.py
+++ main.py
@@ -1,42 +1,56 @@
 import tkinter
 
 # Constant variables
-BCKGRND = "#2C2D2D"
+MAINBACKGROUND = "#2C2D2D"
+
+# Helper Functions
+def createStandardLabel(parent, text, row, column):
+    label = tkinter.Label(
+        parent,
+        text = text,
+        fg = "white",
+        bg = MAINBACKGROUND,
+        font = ("Courier New", 10),
+        pady = 10
+    )
+
+    label.grid(row = row, column = column, sticky = "w")
 
 # Initializes main application window
 window = tkinter.Tk()
 window.title("DFA Generator")
 window.geometry("600x400")
-window.configure(bg = BCKGRND)
+window.configure(bg = MAINBACKGROUND)
 
 # Initialize Main Frame
 frame = tkinter.Frame(
     window,
-    bg = BCKGRND
+    bg = MAINBACKGROUND
 )
 
-frame.pack()
+frame.pack(fill="both", expand=True)
+frame.columnconfigure(0, weight=1)
 
 # Initialize Header
 headerFrame = tkinter.Frame(
     frame, 
-    bg = BCKGRND
+    bg = MAINBACKGROUND
 )
 
 titleLabel = tkinter.Label(
     headerFrame, 
     text = "DFA GENERATOR", 
     fg = "white", 
-    bg = BCKGRND, 
+    bg = MAINBACKGROUND, 
     font = ("Courier New", 20, "bold"),
-    pady = 10
+    pady = 4
 )
 
 subtitleLabel = tkinter.Label(
     headerFrame,
     text = "Separate characters with commas (,)",
     fg = "white",
-    bg = BCKGRND,
+    bg = MAINBACKGROUND,
     font = ("Courier New", 10)
 )
 
@@ -47,48 +61,25 @@ subtitleLabel.grid(row = 1, column = 0)
 # Initialize Formal Inputs Section
 inputsFrame = tkinter.Frame(
     frame,
-    bg = BCKGRND
-)
-
-statesLabel = tkinter.Label(
-    inputsFrame,
-    text = "Input List of States",
-    fg = "white",
-    bg = BCKGRND,
-    font = ("Courier New", 10),
-    pady = 10
+    bg = MAINBACKGROUND
 )
 
 statesInput = tkinter.Entry(inputsFrame)
 
-alphabetLabel = tkinter.Label(
-    inputsFrame,
-    text = "Input Alphabet",
-    fg = "white",
-    bg = BCKGRND,
-    font = ("Courier New", 10),
-    pady = 10
-)
-
 alphabetInput = tkinter.Entry(inputsFrame)
-
-startStateLabel = tkinter.Label(
-    inputsFrame,
-    text = "Input Start State",
-    fg = "white",
-    bg = BCKGRND,
-    font = ("Courier New", 10),
-    pady = 10
-)
 
 startStateInput = tkinter.Entry(inputsFrame)
 
-inputsFrame.grid(row = 1, column = 0)
-statesLabel.grid(row = 0, column = 0)
-statesInput.grid(row = 0, column = 1)
-alphabetLabel.grid(row = 1, column = 0)
-alphabetInput.grid(row = 1, column = 1)
-startStateLabel.grid(row = 1, column = 2)
-startStateInput.grid(row = 1, column = 3)
+inputsFrame.grid(row = 1, column = 0, sticky = "ew")
+inputsFrame.columnconfigure(1, weight = 1)
+
+statesLabel = createStandardLabel(inputsFrame, "Input List of States", 0, 0)
+statesInput.grid(row = 0, column = 1, sticky = "ew", padx = 10)
+
+alphabetLabel = createStandardLabel(inputsFrame, "Input Alphabet", 1, 0)
+alphabetInput.grid(row = 1, column = 1, sticky="ew", padx=10)
+
+startStateLabel = createStandardLabel(inputsFrame, "Input Start State", 2, 0)
+startStateInput.grid(row = 2, column = 1, sticky="ew", padx=10)
 
 window.mainloop()
